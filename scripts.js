27ddce32de261e5f556407aa7b51e57b7b73d5e9
lcm0821 <1921800775@qq.com>
2020-06-25 13:59:41 +0800

    mini-todos 0625最终版

--- scripts.js
+++ scripts.js
@@ -1,13 +1,9 @@
 
-let tasks = []; // {title: "dddddd", done: false}
+let tasks = [];
+let vipitems = 0;
 
 function renderEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input");
-
-    // inputEl.onchange = (e) => {
-    //     console.log("text, ", e.target.value);
-    //     // console.log("input change: ", e);
-    // };
 
     let addTask = () => {
         if (inputEl.value.length === 0) {
@@ -42,7 +38,7 @@ function renderEditor() {
 }
 
 function renderTaskItems() {
-    console.log("reader items");
+    console.log("render items");
     let itemsEl = document.querySelector("#default-todo-panel .todo-items");
 
     itemsEl.querySelectorAll("div").forEach((node) => node.remove());
@@ -74,9 +70,9 @@ function renderTaskItems() {
         let titleEl = document.createElement("label");
         titleEl.innerText = task.title;
         itemEl.append(titleEl);
-        
-        let ctrlbarEl = renderTaskCtrlBar(tasks, i);
- 
+
+        let ctrlbarEl = renderTaskCtrlBar(task, itemEl, i);
+
         itemEl.append(ctrlbarEl);
 
         itemsEl.append(itemEl);
@@ -84,9 +80,43 @@ function renderTaskItems() {
 }
 
 
-function renderTaskCtrlBar(tasks, taskIdx) {
+function renderTaskCtrlBar(task, itemEl, taskIdx) {
     let ctrlbarEl = document.createElement("div");
     ctrlbarEl.className = "ctrlbar";
+
+    let vipEl = document.createElement('input');
+    vipEl.type = "checkbox"
+    vipEl.checked = task.vip;
+    if (task.vip) {
+        itemEl.classList.add("vip")
+    } else {
+        itemEl.classList.remove("vip")
+    }
+    vipEl.onchange = (e) => {
+        task.vip = e.target.checked;
+        if (task.vip) {
+            itemEl.classList.add("vip");
+            let t = task;
+            for (let j = taskIdx; j > 0; j--) {
+                tasks[j] = tasks[j - 1];
+            }
+            tasks[0] = t;
+            vipitems++;
+        } else {
+            itemEl.classList.remove("vip");
+            let t = task;
+            for (let j = taskIdx; j < tasks.length - 1; j++) {
+                tasks[j] = tasks[j + 1];
+            }
+            tasks[tasks.length - 1] = t;
+            vipitems--;
+        }
+        renderTaskItems();
+
+    }
+
+    ctrlbarEl.append(vipEl);
+
 
     let upEl = document.createElement("button");
     if (taskIdx === 0) {
@@ -102,7 +132,7 @@ function renderTaskCtrlBar(tasks, taskIdx) {
     ctrlbarEl.append(upEl);
 
     let downEl = document.createElement("button");
-    if (taskIdx === tasks.length-1) {
+    if (taskIdx === tasks.length - 1) {
         downEl.disabled = true;
     }
 
@@ -114,6 +144,7 @@ function renderTaskCtrlBar(tasks, taskIdx) {
         renderTaskItems();
     };
     ctrlbarEl.append(downEl);
+
 
 
     let cancelEl = document.createElement("button");
